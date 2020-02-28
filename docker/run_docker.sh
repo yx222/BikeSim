ac45c09b1375e3607b03e06edb61fb3946ee8dcd
Yunlong Xu <yunlongxuyx222@gmail.com>
2020-02-27 18:41:40 -0800

    remove hardcoded path in docker script

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,1 +1,2 @@
-docker run --rm -dit --name bikesim -v ~/workspace/BikeSim:/BikeSim -p 8080:8080 yxu/bikesim
+cwd=${PWD%/*} 
+docker run --rm -dit --name bikesim -v $cwd/:/BikeSim -p 8080:8080 yxu/bikesim
